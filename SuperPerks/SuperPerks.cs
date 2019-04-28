a3acd6b528c54496a2d379a1e2f99cfb88d67a53
Slvr99 <bsawii@gmail.com>
2019-04-27 20:02:31 -0400

    Add project files.

---
+++ SuperPerks/SuperPerks.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InfinityScript;
+
+namespace LotD
+{
+    public class LotD : BaseScript
+    {
+        public LotD()
+        {
+            PlayerConnected += LuckyPlayerConnected;
+        }
+        void LuckyPlayerConnected(Entity player)
+        {
+            player.SpawnedPlayer += () => OnPlayerSpawned(player);
+            player.SetField("Tier", 0);
+        }
+        public void OnPlayerSpawned(Entity player)
+        {
+            Gamble(player);
+            CheckPerks(player);
+        }
+        public override void OnPlayerKilled(Entity player, Entity inflictor, Entity attacker, int damage, string mod, string weapon, Vector3 dir, string hitLoc)
+        {
+        }
+        public void Gamble(Entity player)
+        {
+
+        }
+        public void CheckPerks(Entity player)
+        {
+            if (player.Call<bool>("hasperk", "specialty_extraammo"))
+            {
+                player.SetPerk("specialty_extendedmags", true, false);
+            }
+            if (player.Call<bool>("hasperk", "specialty_falldamage"))
+            {
+                //Add earthquake on damage here
+            }
+            if (player.Call<bool>("hasperk", "specialty_delaymine"))
+            {
+                //
+            }
+            if (player.Call<bool>("hasperk", "specialty_fastmantle"))
+            {
+                player.SetPerk("spacialty_automantle", true, false);
+            }
+            if (player.Call<bool>("hasperk", "specialty_selectivehearing"))
+            {
+                //
+            }
+        }
+    }
+}
